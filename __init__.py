0696ac953e425c9307f2a72659173e6677c56e98
C Hamilton <hamiltoncj@users.noreply.github.com>
2020-08-26 12:52:53 -0400

    add simplekml library

--- __init__.py
+++ __init__.py
@@ -1,3 +1,10 @@
+try:
+    import simplekml
+except Exception:
+    import os
+    import site
+    site.addsitedir(os.path.abspath(os.path.dirname(__file__) + '/libs'))
+
 def classFactory(iface):
     from .kmltools import KMLTools
     return KMLTools(iface)
