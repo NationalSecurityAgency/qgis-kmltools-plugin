05123fff5eb6c5aea7c124c97ed3cdbf14efef6a
C Hamilton <hamiltoncj@users.noreply.github.com>
2020-08-27 13:28:38 -0400

    Added epoch time support

--- exportKmz.py
+++ exportKmz.py
@@ -83,7 +83,8 @@ class ExportKmzAlgorithm(QgsProcessingAlgorithm):
                 parentLayerParameterName=self.PrmInputLayer,
                 type=QgsProcessingParameterField.Any,
                 optional=True,
-                allowMultiple=True
+                allowMultiple=True,
+                defaultToAllFields=True
             )
         )
         self.addParameter(
@@ -491,7 +492,8 @@ class ExportKmzAlgorithm(QgsProcessingAlgorithm):
     def exportFields(self, kml_item, fields, f):
         for field in fields:
             v = escape('{}'.format(f[field]).strip())
-            kml_item.extendeddata.newdata(name=field,value=v,displayname=field)
+            if v != '':
+                kml_item.extendeddata.newdata(name=field,value=v,displayname=field)
 
     def setAltitudeMode(self, kml_item, f, alt_mode, mode_field):
         try:
@@ -541,10 +543,18 @@ class ExportKmzAlgorithm(QgsProcessingAlgorithm):
                     day = dt.day()
                     str = '{:04d}-{:02d}-{:02d}'.format(year,month,day)
                     return(str)
+                elif isinstance(dt, float) or isinstance(dt, int):
+                    str = self.prepareEpochTimeString(dt)
+                    return(str)
                 else:
                     s = '{}'.format(dt).strip()
                     if not s:
                         return(None)
+                    try:  # Check for EPOCH Time
+                        str = self.prepareEpochTimeString(float(s))
+                        return(str)
+                    except ValueError:
+                        pass
                     d1 = dateutil.parser.parse(s, default=datetime.datetime(datetime.MINYEAR,1,1,hour=0, minute=0, second=0, microsecond=0, tzinfo=None))
                     d2 = dateutil.parser.parse(s, default=datetime.datetime(datetime.MINYEAR,2,2,hour=1, minute=1, second=1, microsecond=1, tzinfo=None))
                     str = self.prepareDateString(d1, d2)
@@ -619,7 +629,22 @@ class ExportKmzAlgorithm(QgsProcessingAlgorithm):
             s = traceback.format_exc()
             self.feedback.pushInfo(s)
             return(None)
-    
+
+    def prepareEpochTimeString(self, dt):
+        edt = datetime.datetime.fromtimestamp(dt)
+        year = edt.year
+        month = edt.month
+        day = edt.day
+        hour = edt.hour
+        minute = edt.minute
+        second = edt.second
+        microsec = edt.microsecond
+        if microsec == 0:
+            str = '{:04d}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}'.format(year,month,day,hour,minute,second)
+        else:
+            str = '{:04d}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}.{:03d}'.format(year,month,day,hour,minute,second,int(microsec/1000))
+        return(str)
+
     def prepareDateString(self, d1, d2):
         # if only parts of the date are valid then just return those portions
         # otherwise return a fully formatted iso string.
